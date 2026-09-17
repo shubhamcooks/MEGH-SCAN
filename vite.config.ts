ca8df440072657060d43466d4a33e3da2cad2c72
shubhamcooks <xlegendx999@gmail.com>
2026-09-17 20:46:55 +0530

    fix: update config and resolve dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MEGH-SCAN/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
